552341d0d23b77a9f9dc4f71219c74b8b52018bd
Elliot Parrish <eparrish@uoregon.edu>
2016-08-04 11:33:57 +0200

    Adding verilog project

---
+++ VerilogGradientImplementation/SobelFilter.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:29:58 08/04/2016 
+// Design Name: 
+// Module Name:    SobelFilter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module SobelFilter(
+    );
+
+
+endmodule
