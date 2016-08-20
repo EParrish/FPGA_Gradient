130cf86fba1a7d95c28592868356e773ee802b8e
snyderim <ianmsnyder@gmail.com>
2016-08-20 15:26:49 +0200

    Added variable number of bits and cleaned up a bit (added more variables)

--- VerilogGradientImplementation/SobelFilter.vhd
+++ VerilogGradientImplementation/SobelFilter.vhd
@@ -32,11 +32,12 @@ library IEEE;
 --use UNISIM.VComponents.all;
 
 entity SobelFilter is
-    port(   clk, reset: in STD_Logic; --Using clk, might need reset to reset values? not sure
+    generic(n_bits  :   integer := 8);
+    port(clk, reset :   in STD_Logic; --Using clk, might need reset to reset values? not sure
         --in_array  :   in array(8 downto 0) of integer;
         --in_one    :   in std_logic_vector(0 to 2);
-        in11, in12, in13, in21, in22, in23, in31, in32, in33  :   in std_logic_vector(2 downto 0); --might in the future have one bitstream we need to divide up
-        output    :   out std_logic_vector(2 downto 0)
+        in11, in12, in13, in21, in22, in23, in31, in32, in33  :   in std_logic_vector(n_bits-1 downto 0); --might in the future have one bitstream we need to divide up
+        output      :   out std_logic_vector(n_bits-1 downto 0)
         --out_value :   out integer
         --out_one   :   out std_logic_vector(0 to 2)
     );
@@ -44,7 +45,7 @@ end entity;
 
 architecture Behavioral of SobelFilter is
 
-    type    mat is array(2 downto 0, 2 downto 0) of std_logic_vector(2 downto 0);
+    type    mat is array(2 downto 0, 2 downto 0) of std_logic_vector(n_bits-1 downto 0);
     
         
    
@@ -54,26 +55,28 @@ begin
     variable energyMatrix       :   mat;
     variable gradientYMatrix    :   mat;
     variable gradientXMatrix    :   mat;
-    variable Gx                 :   std_logic_vector(2 downto 0) := "000";
-    variable Gy                 :   std_logic_vector(2 downto 0) := "000";
+    variable Gx                 :   std_logic_vector(n_bits-1 downto 0) := (others=>'0');
+    variable Gy                 :   std_logic_vector(n_bits-1 downto 0) := (others=>'0');
     variable GxInt              :   integer := 0;
     variable GyInt              :   integer := 0;
     variable Gtemp              :   integer := 0;
     variable G                  :   integer := 0;
-    variable outputtemp         :   std_logic_vector(2 downto 0) := "000";
+    variable outputtemp         :   std_logic_vector(n_bits-1 downto 0) := (others=>'0');
+    constant makeNegative       :   std_logic_vector(n_bits-1 downto 0) := (0 => '1', others => '0');
+    constant zeroes             :   std_logic_vector(n_bits-1 downto 0) := (others => '0');
     
     begin
  
         gradientYMatrix := (
-            (not(in11)+"001", not(in12(1 downto 0) & "0")+"001", not(in13)+"001"),
-            ("000", "000", "000"),
-            (in31, in32(1 downto 0) & "0", in33)
+            (not(in11)+makeNegative,                    not(in12(1 downto 0) & '0')+makeNegative,   not(in13)+makeNegative),
+            (zeroes,                                    zeroes,                                     zeroes),
+            (in31,                                      in32(1 downto 0) & '0',                     in33)
         );
     
         gradientXMatrix := (
-            (not(in11)+"001", "000", in13),
-            (not(in21(1 downto 0) & "0")+"001", "000", in23(1 downto 0) & "0"),
-            (not(in31)+"001", "000", in33)
+            (not(in11)+makeNegative,                    zeroes,                                     in13),
+            (not(in21(1 downto 0) & '0')+makeNegative,  zeroes,                                     in23(1 downto 0) & '0'),
+            (not(in31)+makeNegative,                    zeroes,                                     in33)
         );
     
         if rising_edge(clk) then
@@ -86,14 +89,14 @@ begin
             end loop;
         end if;
     
-        GxInt := to_integer(signed(Gx));
-        GyInt := to_integer(signed(Gy));
+        GxInt       :=  to_integer(signed(Gx));
+        GyInt       :=  to_integer(signed(Gy));
         
-        Gtemp := GxInt*GxInt + GyInt*GyInt;
-        outputtemp  := std_logic_vector(to_unsigned(Gtemp, 3)); --if testbed failes then maybe move this outside process
-        output <= "00" & outputtemp(0 downto 0);
+        Gtemp       :=  GxInt*GxInt + GyInt*GyInt;
+        outputtemp  :=  std_logic_vector(to_unsigned(Gtemp, n_bits)); --if testbed failes then maybe move this outside process
+        output      <=  "00" & outputtemp(n_bits-1 downto 2);
         --variable energyValues is std_logic_vector(0 to 32, 0 to 32);
         --variable yfilter is std_logic_vector(0 to 2, 0 to 2);
-
     end process;
+    --output      <=  "00" & outputtemp(nbits-1 downto 2);
 end architecture;
