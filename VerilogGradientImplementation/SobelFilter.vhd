b5faf98829cf624e87465480d97aa287459165bc
snyderim <ianmsnyder@gmail.com>
2016-08-23 13:07:16 +0200

    Updated algorithm to get rid of matrice addition, fixed test bench

--- VerilogGradientImplementation/SobelFilter.vhd
+++ VerilogGradientImplementation/SobelFilter.vhd
@@ -32,10 +32,10 @@ library IEEE;
 --use UNISIM.VComponents.all;
 
 entity SobelFilter is
-    generic(n_bits  :   integer := 8);
+    generic(n_bits  :   integer := 12);
     port(clk, reset :   in STD_Logic; --Using clk, might need reset to reset values? not sure
         in11, in12, in13, in21, in22, in23, in31, in32, in33  :   in std_logic_vector(n_bits-1 downto 0); --might in the future have one bitstream we need to divide up
-        output      :   out std_logic_vector(n_bits-1 downto 0)
+        output      :   out std_logic_vector(n_bits+1 downto 0)
     );
 end entity;
 
@@ -57,14 +57,14 @@ begin
     variable GyInt              :   integer;-- := 0;
     variable Gtemp              :   integer;-- := 0;
     variable G                  :   integer;-- := 0;
-    variable outputtemp         :   std_logic_vector(n_bits-1 downto 0);-- := (others=>'0');
+    variable outputtemp         :   std_logic_vector(n_bits+3 downto 0);-- := (others=>'0');
     constant makeNegative       :   std_logic_vector(n_bits-1 downto 0) := (0 => '1', others => '0');
     constant zeroes             :   std_logic_vector(n_bits-1 downto 0) := (others => '0');
     
     begin
- 
+-- Matrices don't work, but here's a schematic of them: 
 --        gradientYMatrix := (
---            (std_logic_vector(signed(not(in11))+signed(makeNegative)),  std_logic_vector(signed(not(in12(1 downto 0) & '0'))+signed(makeNegative)),     std_logic_vector(signed(not(in13))+signed(makeNegative))),
+--            (std_logic_vector(signed(not(in11))+signed(makeNegative)),  std_logic_vector(signed(not(in12(n_bits-2 downto 0) & '0'))+signed(makeNegative)),     std_logic_vector(signed(not(in13))+signed(makeNegative))),
 --            (zeroes,                                                    zeroes,                                                                         zeroes),
 --            (in31,                                                      in32(n_bits-2 downto 0) & '0',                                                  in33)
 --        );
@@ -87,8 +87,10 @@ begin
               --      Gy := std_logic_vector(signed(Gy) + signed(gradientYMatrix(i,j)));
                -- end loop;
             --end loop;
-        Gx := std_logic_vector((signed(not(in11))+signed(makeNegative)) + (signed(not(in12(1 downto 0) & '0')))+signed(makeNegative)) + (signed(not(in13))+signed(makeNegative)) + signed(in31) + signed(in32(n_bits-2 downto 0) & '0') + signed(in33));
-        Gy := std_logic_vector((signed(not(in11))+signed(makeNegative)) + signed(in13) + (signed(not(in21(n_bits-2 downto 0) & '0'))+signed(makeNegative)) + signed(in23(n_bits-2 downto 0) & '0')) + (signed(not(in31))+signed(makeNegative)) + signed(in33));
+        Gx := std_logic_vector((signed(not(in11))+signed(makeNegative)) + (signed(not(in12(n_bits-2 downto 0) & '0'))+signed(makeNegative)) + (signed(not(in13))+signed(makeNegative)) + signed(in31) + signed(in32(n_bits-2 downto 0) & '0') + signed(in33));
+        --Gx := std_logic_vector(signed(in33)-signed(in11)-signed(in12(n_bits-2 downto 0) & '0')-signed(in13) + signed(in31) + signed(in32(n_bits-2 downto 0) & '0'));
+
+        Gy := std_logic_vector((signed(not(in11))+signed(makeNegative)) + signed(in13) + (signed(not(in21(n_bits-2 downto 0) & '0'))+signed(makeNegative)) + signed(in23(n_bits-2 downto 0) & '0') + (signed(not(in31))+signed(makeNegative)) + signed(in33));
         
         --end if;
     
@@ -96,8 +98,8 @@ begin
         GyInt       :=  to_integer(signed(Gy));
         
         Gtemp       :=  GxInt*GxInt + GyInt*GyInt;
-        outputtemp  :=  std_logic_vector(to_unsigned(Gtemp, n_bits)); --if testbed failes then maybe move this outside process
-        output      <=  outputtemp;--'0' & '0' & outputtemp(n_bits-1 downto 2);
+        outputtemp  :=  std_logic_vector(to_unsigned(Gtemp, n_bits+4)); --if testbed failes then maybe move this outside process
+        output      <=  outputtemp(n_bits+3 downto 2);
         
 --        else 
 --            output <= (others => '0');
